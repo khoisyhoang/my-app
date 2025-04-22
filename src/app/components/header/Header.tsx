8656cc10da5d7d5e0f934f5cd1f80b2e016a9664
Khoi Hoang <khoisyhoang@gmail.com>
2025-04-22 10:37:55 -0400

    navbar

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,26 +1,30 @@
 import HomeIcon from '@mui/icons-material/Home';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import Link from 'next/link';
 
 export default function Header() {
 
     return (
-        <header className="shadow-md bg-[#98C1D9]">
-            <div className="flex justify-between items-center w-[1280px] mx-auto gap-[60px] p-3">
-                <h1 className="text-sm sm:text-xl flex items-center">
-                    <HomeIcon />
-                    <div>Estate</div>
+        <header className="shadow-md bg-[#98C1D9] ">
+            <div className="flex justify-between items-center container md:gap-[60px] gap-[25px] p-3 mx-auto">
+                <h1 className="text-[16px] sm:text-[24px]  ">
+                    <Link href={"/"} className='flex items-center gap-[5px'>
+                        <HomeIcon />
+                        <div>Estate</div>
+                    </Link>
+
                 </h1>
                 <form action="" className="flex gap-[10px] flex-1">
-                    <input type="text" placeholder="Search..." className='bg-[#212121] rounded-[50px] px-[10px] py-[3px] flex-1' />
+                    <input type="text" placeholder="Search..." className='bg-[#BBC7CE] rounded-[50px] px-[10px] py-[3px] flex-1' />
                     <button type="submit" className="border-white border-2 rounded-[10px] p-1">Search</button>
                 </form>
-                <ul className="flex justify-center items-center gap-[30px]">
-                    <li>
+                <ul className="flex justify-center items-center gap-[30px] text-[20px]">
+                    <li className='hidden sm:inline'>
                         <Link href={"/"}>
                             Home
                         </Link>
                     </li>
-                    <li>
+                    <li className='hidden sm:inline'>
                         <Link href={"/about"}>
                             About
                         </Link>
